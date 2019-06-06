3841f031d8c76aa04c0ad1278083656133467efa
Chris Buckley <chris.buckley@rackspace.com>
2019-06-06 16:16:11 +1000

    Testing for expected failure

--- faws_standards_tests.py
+++ faws_standards_tests.py
@@ -13,8 +13,8 @@ class TestBaseNetworkStandards(unittest.TestCase):
 
     def test_vpc_cidr(self):
         #ensure that VPC module's CIDR range is RFC-1918 and AWS CIDR range compliant
-        #self.v.resources("vpc").property("cidr_range").should_match_regex("10\.0\.0\.0\/8")
-        self.v.resources("vpc").property("cidr_range").should_match_regex("(?:(?:^10\.)|(?:^172\.1[6-9]\.)|(?:^172\.2[0-9]\.)|(?:^172\.3[0-1]\.)|(?:^192\.168\.))(?:[0-9]{1,2}[0-5]{0,1}\.)(?:[0-9]{1,2}[0-5]{0,1}\.)(?:[0-9]{1,2}[0-5]{0,1})\/(?:1(?:[6-9]))|(?:2(?:[0-8]))$")
+        self.v.resources("vpc").property("cidr_range").should_match_regex("testvalue")
+        #self.v.resources("vpc").property("cidr_range").should_match_regex("(?:(?:^10\.)|(?:^172\.1[6-9]\.)|(?:^172\.2[0-9]\.)|(?:^172\.3[0-1]\.)|(?:^192\.168\.))(?:[0-9]{1,2}[0-5]{0,1}\.)(?:[0-9]{1,2}[0-5]{0,1}\.)(?:[0-9]{1,2}[0-5]{0,1})\/(?:1(?:[6-9]))|(?:2(?:[0-8]))$")
 
     def test_az_count(self):
         #ensure that the VPC is configured to span at least two AZs
