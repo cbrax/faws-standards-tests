4b2c3045206b370a65e16254c60a44210928f477
Chris Buckley <chris.buckley@rackspace.com>
2019-06-06 16:11:19 +1000

    Correct testing of VPC CIDR range

--- faws_standards_tests.py
+++ faws_standards_tests.py
@@ -13,7 +13,8 @@ class TestBaseNetworkStandards(unittest.TestCase):
 
     def test_vpc_cidr(self):
         #ensure that VPC module's CIDR range is RFC-1918 and AWS CIDR range compliant
-        self.v.resources("vpc").property("cidr_range").should_match_regex("10.0.0.0/16")
+        #self.v.resources("vpc").property("cidr_range").should_match_regex("10\.0\.0\.0\/8")
+        self.v.resources("vpc").property("cidr_range").should_match_regex("(?:(?:^10\.)|(?:^172\.1[6-9]\.)|(?:^172\.2[0-9]\.)|(?:^172\.3[0-1]\.)|(?:^192\.168\.))(?:[0-9]{1,2}[0-5]{0,1}\.)(?:[0-9]{1,2}[0-5]{0,1}\.)(?:[0-9]{1,2}[0-5]{0,1})\/(?:1(?:[6-9]))|(?:2(?:[0-8]))$")
 
     def test_az_count(self):
         #ensure that the VPC is configured to span at least two AZs
@@ -22,7 +23,6 @@ class TestBaseNetworkStandards(unittest.TestCase):
     #def test_public_subnet_count(self):
     #    #ensure that the VPC has at least two public subnets
     #    self.v.resources("vpc").property("public_cidr_range").
-
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestBaseNetworkStandards)
